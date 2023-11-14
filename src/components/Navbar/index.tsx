ecf8aad48f52d6a9ed046352d71c2f36668216e5
Gurkirat Singh <singh.gurkirat2707@gmail.com>
2023-11-14 16:39:27 -0500

    Add home, generate, verify pages

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,30 +1,37 @@
-"use client"
+'use client'
 import React from 'react'
 
-import { usePathname, useRouter } from 'next/navigation';
-import Link from 'next/link';
-
+import { usePathname, useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export function Navbar() {
-  const router = useRouter();
+  const router = useRouter()
 
-  const pathname = usePathname();
+  const pathname = usePathname()
 
-  if (pathname.startsWith("/certificate/")) {
+  if (pathname.startsWith('/certificate/')) {
     return <></>
   }
 
   return (
-    <nav className="w-full flex justify-between  py-4 h-[12vh] xl:px-20 md:px-14 px-8">
-      <span onClick={() => router.push("/")} className=" cursor-pointer flex items-center justify-center gap-4 xl:ml-8 md:ml-4 ml-2">
-        <h1 className="md:text-3xl  text-xl secondary_gradient font-bold ">100xdevs cohort Certificates</h1>
+    <nav className="w-full md:flex justify-between space-y-4 items-center py-4 h-[12vh] ">
+      <span
+        onClick={() => router.push('/')}
+        className=" cursor-pointer flex items-center justify-center gap-4 xl:ml-8 md:ml-4 ml-2"
+      >
+        <h1 className="md:text-3xl !underline text-xl secondary_gradient font-bold ">
+          100xDevs Cohort Certificates
+        </h1>
       </span>
       <span className="flex items-center justify-center gap-4">
-        <Link href={"/email"}>
-          <button className="btn_primary_1" >Get Certificate</button>
+        <Link href={'/generate'}>
+          <button className="btn_primary_1">Generate Certificate</button>
+        </Link>
+        <Link href={'/verify'}>
+          <button className="btn_primary_1">Verify Certificate</button>
         </Link>
         <a href="https://harkirat.classx.co.in/new-courses/2" target="_blank">
-          <button className="btn_primary_1">Link to cohort</button>
+          <button className="btn_secondary_2">Go to cohort</button>
         </a>
       </span>
     </nav>
